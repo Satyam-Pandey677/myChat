2ff8adf56464fed3e10cf59ea133c53e47580a85
Satyam-Pandey677 <satyampandey0241@gmail.com>
2026-02-11 14:37:17 +0530

    chaning things

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,16 +1,22 @@
 import express from "express"
 import { dbConnect } from "./config/DB.js";
+import {createClient} from "redis"
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 8000;
 
 
-dbConnect()
+dbConnect();
 
+export const redisClient = createClient({
+    url:`${process.env.REDIS_URL}`,
+});
 
-app.route("/").get((req, res) => {
-    res.send("hello there")
-})
+redisClient.connect().then(() => console.log("connnected to redis"))
+
+import userRoute from "./routes/userRouter.js"
+
+app.use("api/v1/user",userRoute)
 
 app.listen(port, () => {
     console.log(`server running on : http://localhost:${port}`)
