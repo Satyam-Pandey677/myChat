639f9779890fbcff3b6bd7f5ee50ffa8db26462a
Satyam-Pandey677 <satyampandey0241@gmail.com>
2026-02-19 11:38:01 +0530

    feat: add login and verification pages with email OTP functionality
    - Created a global CSS file and imported Tailwind CSS.
    - Implemented the RootLayout component for consistent layout structure.
    - Developed the LoginPage component for user email input and OTP request.
    - Added a redirect page to navigate users to the chat page.
    - Created the VerifyPage component for OTP verification with email display.
    - Added a basic VerifyOTP component for future OTP handling.
    - Configured TypeScript settings with a new tsconfig.json file.

---
+++ frontend/src/app/chat/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatApp = () => {
+  return (
+    <div>Chat app</div>
+  )
+}
+
+export default ChatApp
---
+++ frontend/src/components/verifyOTP.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VerifyOTP = () => {
+  return (
+    <div>V</div>
+  )
+}
+
+export default VerifyOTP
